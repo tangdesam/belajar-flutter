cb174060015c62176a958fd93393453a1eb137a6
ilufa <tangdesam@gmail.com>
2022-09-28 16:59:53 +0700

    initial init PR match_soccer

---
+++ match_soccer/android/app/src/main/kotlin/com/lab/match_soccer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lab.match_soccer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ match_soccer/lib/list_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListPage extends StatelessWidget {
+  ListPage({Key? key}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Match Soccer'),
+      ),
+      body: const Text('body'),
+    );
+  }
+}
