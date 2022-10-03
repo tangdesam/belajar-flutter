44711fb6edd1d19384f78383a89df89c88227ffe
ilufa <tangdesam@gmail.com>
2022-10-03 09:47:09 +0700

    tombol back, dengan Stack

--- meet3/lib/detail_page.dart
+++ meet3/lib/detail_page.dart
@@ -4,7 +4,9 @@ import 'package:meet3/model/game_info.dart';
 
 class DetailPage extends StatelessWidget {
   final GameInfo gameInfo;
-  const DetailPage({Key? key, required GameInfo this.gameInfo}): super(key: key);
+
+  const DetailPage({Key? key, required GameInfo this.gameInfo})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +16,32 @@ class DetailPage extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Image.asset(gameInfo.banner),
+              // Image.asset(gameInfo.banner),
+              Stack(
+                children: <Widget>[
+                  Image.asset(gameInfo.banner),
+                  SafeArea(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          CircleAvatar(
+                            backgroundColor: Colors.grey,
+                            child: IconButton(
+                              icon: const Icon(Icons.arrow_back),
+                              color: Colors.white,
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
               Container(
                 margin: const EdgeInsets.only(top: 16, left: 16),
                 child: Text(gameInfo.name,
@@ -26,7 +53,8 @@ class DetailPage extends StatelessWidget {
                 ),
               ),
               Container(
-                margin: const EdgeInsets.symmetric(vertical: 16, horizontal: 48),
+                margin: const EdgeInsets.symmetric(
+                    vertical: 16, horizontal: 48),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
@@ -56,7 +84,8 @@ class DetailPage extends StatelessWidget {
               ),
               Container(
                 margin: const EdgeInsets.only(top: 16, left: 16),
-                child: const Text("StarCraft is a military science fiction media franchise created by Chris Metzen and James Phinney and owned by Blizzard Entertainment.[1] The series, set in the beginning of the 26th century, centers on a galactic struggle for dominance among four species—the adaptable and mobile Terrans, the ever-evolving insectoid Zerg, the powerful and enigmatic Protoss, and the godlike Xel'Naga creator race—in a distant part of the Milky Way galaxy known as the Koprulu Sector. The series debuted with the video game StarCraft in 1998. It has grown to include a number of other games as well as eight novelizations, two Amazing Stories articles, a board game, and other licensed merchandise such as collectible statues and toys. ",
+                child: const Text(
+                  "StarCraft is a military science fiction media franchise created by Chris Metzen and James Phinney and owned by Blizzard Entertainment.[1] The series, set in the beginning of the 26th century, centers on a galactic struggle for dominance among four species—the adaptable and mobile Terrans, the ever-evolving insectoid Zerg, the powerful and enigmatic Protoss, and the godlike Xel'Naga creator race—in a distant part of the Milky Way galaxy known as the Koprulu Sector. The series debuted with the video game StarCraft in 1998. It has grown to include a number of other games as well as eight novelizations, two Amazing Stories articles, a board game, and other licensed merchandise such as collectible statues and toys. ",
                   style: TextStyle(
                     fontSize: 10,
                   ),
@@ -66,41 +95,41 @@ class DetailPage extends StatelessWidget {
               SizedBox( // pembungkus untuk fix height
                 height: 150,
                 child:
-                  ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: gameInfo.imageUrls.map((url) {
-                        return Padding(
-                          padding: const EdgeInsets.all(4.0),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(10.0),
-                            child:  Image.network(url),
-                          ),
-                        );
-                    }).toList(),
-                    // children: [
-                    //   Padding(
-                    //     padding: const EdgeInsets.all(4.0),
-                    //     child: ClipRRect(
-                    //       borderRadius: BorderRadius.circular(10.0),
-                    //       child:  Image.network('https://cdn.vox-cdn.com/thumbor/EBt6HUM8C3Ro4mTRFXR76EI47e4=/0x0:1920x1080/1200x800/filters:focal(252x242:558x548)/cdn.vox-cdn.com/uploads/chorus_image/image/55523673/Game2_Space_ProtossRush_SpectatorPerspective_01.0.jpg'),
-                    //     ),
-                    //   ),
-                    //   Padding(
-                    //     padding: const EdgeInsets.all(4.0),
-                    //     child: ClipRRect(
-                    //       borderRadius: BorderRadius.circular(10.0),
-                    //       child: Image.network('https://www.gamespot.com/a/uploads/original/123/1239113/3105107-184087-starcraft.png'),
-                    //     ),
-                    //   ),
-                    //   Padding(
-                    //     padding: const EdgeInsets.all(4.0),
-                    //     child: ClipRRect(
-                    //       borderRadius: BorderRadius.circular(10.0),
-                    //       child: Image.network('https://assets.reedpopcdn.com/starcraft-remastered-out-this-summer-1490599238622.jpg/BROK/thumbnail/1600x900/format/jpg/quality/80/starcraft-remastered-out-this-summer-1490599238622.jpg'),
-                    //     ),
-                    //   ),
-                    // ],
-                  ),
+                ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: gameInfo.imageUrls.map((url) {
+                    return Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10.0),
+                        child: Image.network(url),
+                      ),
+                    );
+                  }).toList(),
+                  // children: [
+                  //   Padding(
+                  //     padding: const EdgeInsets.all(4.0),
+                  //     child: ClipRRect(
+                  //       borderRadius: BorderRadius.circular(10.0),
+                  //       child:  Image.network('https://cdn.vox-cdn.com/thumbor/EBt6HUM8C3Ro4mTRFXR76EI47e4=/0x0:1920x1080/1200x800/filters:focal(252x242:558x548)/cdn.vox-cdn.com/uploads/chorus_image/image/55523673/Game2_Space_ProtossRush_SpectatorPerspective_01.0.jpg'),
+                  //     ),
+                  //   ),
+                  //   Padding(
+                  //     padding: const EdgeInsets.all(4.0),
+                  //     child: ClipRRect(
+                  //       borderRadius: BorderRadius.circular(10.0),
+                  //       child: Image.network('https://www.gamespot.com/a/uploads/original/123/1239113/3105107-184087-starcraft.png'),
+                  //     ),
+                  //   ),
+                  //   Padding(
+                  //     padding: const EdgeInsets.all(4.0),
+                  //     child: ClipRRect(
+                  //       borderRadius: BorderRadius.circular(10.0),
+                  //       child: Image.network('https://assets.reedpopcdn.com/starcraft-remastered-out-this-summer-1490599238622.jpg/BROK/thumbnail/1600x900/format/jpg/quality/80/starcraft-remastered-out-this-summer-1490599238622.jpg'),
+                  //     ),
+                  //   ),
+                  // ],
+                ),
               ),
             ],
           ),
